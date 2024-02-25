1fcaf5538f64be893e88a2edae6d56d5b67c5f6c
Shane Taylor <sbtaylor789@gmail.com>
2024-02-25 18:12:17 -0500

    added code to stream .m3u8 links to website

--- GeckoCam/src/pages/Gecko.jsx
+++ GeckoCam/src/pages/Gecko.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import video from "/test.mp4";
 
 const Gecko = () => {
   return (
-    <div>
-      <h1>Gecko</h1>
-    </div>
+    <video
+      id="my-video"
+      class="video-js"
+      controls
+      width="1280"
+      height="720"
+      autoPlay
+      muted
+      data-setup="{}"
+    >
+      <source
+        src="http://10.0.0.189/hls/stream.m3u8"
+        type="application/x-mpegURL"
+      />
+    </video>
   );
 };
 
